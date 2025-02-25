2c85cb0c71d5f44e3eb40a6b8c6bc13a94fc23d9
山荣火斤口口 <lv_xinrong@outlook.com>
2025-02-25 22:09:57 +0800

    add code

---
+++ ch02/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"go_jvm/ch02/classpath"
+	"strings"
+)
+
+func main() {
+	cmd := parseCmd()
+	if cmd.versionFlag {
+		fmt.Println("version 0.0.1")
+	} else if cmd.helpFlag || cmd.class == "class" {
+		printUsage()
+	} else {
+		startJVM(cmd)
+	}
+}
+
+func startJVM(cmd *Cmd) {
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	fmt.Printf("classpath: %v clss:%v args:%v\n", cp, cmd.class, cmd.args)
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	classData, _, err := cp.ReadClass(className)
+	if err != nil {
+		fmt.Printf("Cloud not find or load main class %s\n", cmd.class)
+		return
+	}
+	fmt.Printf("class data:%v\n", classData)
+}
